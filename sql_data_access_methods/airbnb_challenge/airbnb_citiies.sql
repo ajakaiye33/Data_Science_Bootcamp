3a1322cd6f9508422c2eb685be09164e86416a04
Hedgar <ajakzhedgar@gmail.com>
2019-01-17 22:23:25 +0100

    raping up

--- sql_data_access_methods/airbnb_challenge/airbnb_citiies.sql
+++ sql_data_access_methods/airbnb_challenge/airbnb_citiies.sql
@@ -40,3 +40,48 @@ ORDER BY
 
 
 --What time of year is the cheapest time to go to your city
+
+
+SELECT
+	c.date,
+	l.price,
+	MIN(l.price) as cheapest_price,
+	date_part('month', c.date) as month_of_year
+
+FROM
+    clean_lond_cal c
+
+JOIN
+    london_listings l
+
+ON l.id = c.listing_id
+
+WHERE
+    l.price >=5
+
+GROUP BY 1,2
+
+ORDER BY
+    cheapest_price, month_of_year ASC;
+
+
+
+--What about the busiest?
+
+SELECT
+    l.availability_365,
+    c.date,
+    MAX(l.calculated_host_listings_count),
+
+
+FROM
+    london_listings l
+
+JOIN
+    clean_lond_cal c
+
+ON l.id = c.listing_id
+
+GROUP BY 1,2
+
+ORDER BY l.availability_365;
